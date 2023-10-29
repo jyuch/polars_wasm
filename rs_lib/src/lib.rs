6c397a9ffd56d697f5205cc0efe6c71b5798dd1d
jyuch <jyuch@users.noreply.github.com>
2023-10-29 18:44:53 +0900

    wasmを使ってブラウザでcsvを読み込む

--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -1,4 +1,5 @@
 use polars::prelude::*;
+use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,4 +9,16 @@ pub fn add_vector(x: Vec<i32>, y: Vec<i32>) -> Option<Vec<i32>> {
 
   let result = (&x + &y).i32().ok()?.into_no_null_iter().collect();
   Some(result)
+}
+
+#[wasm_bindgen]
+pub fn load_csv_and_sum(csv: Vec<u8>) -> Option<i32> {
+  let df = CsvReader::new(Cursor::new(csv)).finish().ok()?;
+  let df = df
+    .clone()
+    .lazy()
+    .select([col("value").sum()])
+    .collect()
+    .ok()?;
+  Some(df.get_row(0).ok()?.0.iter().nth(0)?.try_extract().ok()?)
 }
